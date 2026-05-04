e7f41f71c108071849a9f6b628f3488196aaf8ca
pboyd-oss <pboyd@tuxgrid.com>
2026-05-04 18:24:29 +0100

    Use Jenkins credential store for registry auth

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,5 +1,15 @@
 def call(Map config = [:]) {
-    container('skaffold') {
-        sh 'skaffold build --file-output=artifacts.json'
+    withCredentials([usernamePassword(
+        credentialsId: 'gitea-registry',
+        usernameVariable: 'REG_USER',
+        passwordVariable: 'REG_PASS'
+    )]) {
+        container('skaffold') {
+            sh """
+                mkdir -p /tmp/.docker
+                echo '{"auths":{"${env.REGISTRY.split('/')[0]}":{"auth":"'"\$(echo -n \$REG_USER:\$REG_PASS | base64)"'"}}}' > /tmp/.docker/config.json
+                DOCKER_CONFIG=/tmp/.docker skaffold build --file-output=artifacts.json
+            """
+        }
     }
 }
